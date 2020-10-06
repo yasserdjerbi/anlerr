12b0522042a68a035afb2a1f157ebf26949156de
Abdullah Altaezi <42510452+taeziaa@users.noreply.github.com>
2020-10-06 16:02:12 +0300

    Add nuro_pos_receipt

---
+++ nuro_pos_receipt/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# Part of Nuro Solution Pvt Ltd.
+""" init file """
---
+++ nuro_pos_receipt/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Part of Nuro Solution Pvt Ltd.
+{
+    'name': 'POS New Receipt',
+    'version': '13.0.1.0.0',
+    'summary': """Pos Custom Receipt change odoo POS receipt in table format""",
+    'description': """
+                    Change Odoo POS Receipt
+                    Odoo POS Receipt table format
+                    New Odoo POS Receipt                                      
+                    """,
+    'author': "Nurosolution pvt Ltd",
+    'category': 'Sales/Point Of Sale',
+    'website': 'nurosolution.com',
+    'support': 'info@nurosolution.com',
+    'company': 'Nurosolution Pvt Ltd',
+    'license': 'OPL-1',
+    'images': ['static/description/banner.png'],
+    'depends': ['base', 'point_of_sale'],
+    "data": [],
+    'demo': [],
+    'qweb': ['static/src/xml/pos.xml'],
+    'installable': True,
+}
